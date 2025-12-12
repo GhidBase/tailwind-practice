c79f63fbc233510e701aaed3c8c3b831a784b646
Dylan Simon <grvdylans@gmail.com>
2025-12-12 16:46:06 -0700

    Reimported app.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./App.css";
 import Checklist from "./components/Checklist.jsx";
 
 createRoot(document.getElementById("root")).render(
