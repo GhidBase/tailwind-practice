9f94ed237e29bb62709d4835ed1d66a59c735f71
Dylan Simon <grvdylans@gmail.com>
2025-12-16 12:50:40 -0700

    Finish page creation and deletion. Widen desktop version

--- src/components/PageManager.jsx
+++ src/components/PageManager.jsx
@@ -14,16 +14,50 @@ export default function PageManager() {
             .then((result) => setPages(result));
     }, []);
 
-    function createPage(title) {
+    async function createPage(title) {
         if (!title?.trim()) {
             console.log("Error - title cannot be empty");
             return;
         }
 
+        let response;
+        try {
+            response = await fetch("http://localhost:3000/pages", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ title }),
+            });
+        } catch (err) {
+            console.error("Error", err);
+            return;
+        }
+
+        if (!response.ok) {
+            try {
+                await response.json();
+            } catch {
+                ("");
+            }
+            console.error("Create page failed");
+            return;
+        }
+
+        const newPage = await response.json();
+        const newPages = [...pages];
+        newPages.push(newPage);
+        setPages(newPages);
+    }
+
+    function deletePage(id) {
+        if (!+id) {
+            console.log("Error - Invalid ID " + +id);
+            return;
+        }
+
         fetch("http://localhost:3000/pages", {
-            method: "POST",
+            method: "DELETE",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ title }),
+            body: JSON.stringify({ id }),
         });
     }
 
@@ -35,24 +69,44 @@ export default function PageManager() {
                 <div className="p-4">
                     <h1>Admin Tools</h1>
 
-                    <div className="flex justify-between items-center mx-auto">
+                    <div className="flex justify-between items-center mx-auto gap-2">
                         <h1>Pages:</h1>
                         <input
                             type="text"
-                            className="bg-neutral-200 text-black px-2 box-border rounded ml-auto mr-2"
+                            id="title"
+                            name="title"
+                            className="bg-neutral-200 min-w-0 text-black px-2 box-border rounded ml-auto"
                             onChange={(e) => setTitle(e.target.value)}
                             value={title}
                         />
                         <button
                             onClick={() => createPage(title)}
-                            className="text-amber-50 bg-neutral-600 rounded px-2 py-0.5"
+                            className="text-amber-50 bg-neutral-600 w-38 rounded px-2 py-0.5"
                         >
                             Create Page
                         </button>
                     </div>
-                    <ul>
-                        {pages.map((page) => {
-                            return <li>{page.title}</li>;
+                    <ul className="">
+                        {pages.map((page, pageIndex) => {
+                            return (
+                                <li
+                                    className="mt-4 w-full flex justify-between"
+                                    key={page.id}
+                                >
+                                    <p>{page.title}</p>
+                                    <button
+                                        onClick={() => {
+                                            const newPages = [...pages];
+                                            newPages.splice(pageIndex, 1);
+                                            setPages(newPages);
+                                            deletePage(page.id);
+                                        }}
+                                        className="text-amber-50 bg-neutral-600 w-22 rounded px-2 py-0.5"
+                                    >
+                                        Delete
+                                    </button>
+                                </li>
+                            );
                         })}
                     </ul>
                 </div>
